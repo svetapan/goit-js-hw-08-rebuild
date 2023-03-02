a604d07101f9bc997d66113b35f8619a837c3169
Svetlana Panenko <svetabagira1986@gmail.com>
2023-03-02 21:21:30 +0200

    fixed Vimeo player

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,20 @@
 import Vimeo from '@vimeo/player';
+import throttle from "lodash.throttle";
 
-const throttle = require('lodash.throttle');
 const iframe = document.querySelector('iframe');
 const player = new Vimeo(iframe);
+const savedVideoplayerCurrentTime = localStorage.getItem('videoplayer-current-time');
 
+player.on('timeupdate', throttle(restorePosition, 1000));
+    
+function restorePosition (data) {
+    localStorage.setItem('videoplayer-current-time', data.seconds)
+};
 
-player.on('timeupdate', throttle(function(data) {
-    localStorage.setItem('videoplayer-current-time', data.seconds);
-}, 1000));
+populateLocalStorage();
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'))
+function populateLocalStorage() {
+    if (savedVideoplayerCurrentTime) {
+        player.setCurrentTime(savedVideoplayerCurrentTime);
+    }
+}
